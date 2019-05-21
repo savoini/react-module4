ed4c060357c47340c91140ee9426b6dc3c61979f
Gilberto <savoini@dac246.dac.unicamp.br>
2019-05-21 19:28:46 -0300

    Player

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Global from './styles/global';
+import { Wrapper, Container } from './styles/components';
 
 import Sidebar from './components/Sidebar';
+import Player from './components/Player';
 
 function App() {
   return (
-    <>
+    <Wrapper>
       <Global />
-      <Sidebar />
-    </>
+      <Container>
+        <Sidebar />
+      </Container>
+      <Player />
+    </Wrapper>
   );
 }
 
